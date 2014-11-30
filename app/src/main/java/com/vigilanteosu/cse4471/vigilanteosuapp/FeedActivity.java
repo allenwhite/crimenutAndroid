b54ad7e0abd8d496148902cbb7a7c702b30d2779
Neil Madsen <neilmadsen@gmail.com>
2014-11-30 17:23:29 -0500

    Started news feed layout

--- app/src/main/java/com/vigilanteosu/cse4471/vigilanteosuapp/FeedActivity.java
+++ app/src/main/java/com/vigilanteosu/cse4471/vigilanteosuapp/FeedActivity.java
@@ -1,19 +1,35 @@
 package com.vigilanteosu.cse4471.vigilanteosuapp;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+
+import java.util.HashMap;
 
 
-public class FeedActivity extends Activity {
-
+public class FeedActivity extends ListActivity {
+    private final String getReportsUrl = "http://jeffcasavant.com:10100/vig/api/v1.0/list/reports?";
+    private final String numPosts = "15";
     SessionManagement session;
-/* WE NEED BUTTONS TO GET TO THE OTHER SCREEN... ALSO MIGHT BE OF VALUE TO CHECK THAT
-THE TOKEN IS VALID (METHOD DNE RN THO)*/
+    /* WE NEED BUTTONS TO GET TO THE OTHER SCREEN... ALSO MIGHT BE OF VALUE TO CHECK THAT
+    THE TOKEN IS VALID (METHOD DNE RN THO)*/
 
+    private HashMap<String, String>[] getReports(String reportUUID){
+        HashMap<String, String>[] reports = new HashMap[15];
+        SharedPreferences pref =
+                getSharedPreferences(SessionManagement.PREF_NAME,
+                        MODE_PRIVATE);
+        String token = pref.getString("token", "");
+        // If the token is not set
+        if(token.equals("")){
+            //TODO error
+        }
+        String url = createUrl(token, reportUUID, numPosts);
+        return reports;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +46,6 @@ THE TOKEN IS VALID (METHOD DNE RN THO)*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.feed, menu);
         return true;
@@ -43,10 +57,7 @@ THE TOKEN IS VALID (METHOD DNE RN THO)*/
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
 
