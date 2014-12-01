429e86ca9bcb243941fb1a4d8e6657c6d5312735
Neil Madsen <neilmadsen@gmail.com>
2014-12-01 07:27:56 -0500

    Implemented rough version of feed

--- app/src/main/java/com/vigilanteosu/cse4471/vigilanteosuapp/FeedActivity.java
+++ app/src/main/java/com/vigilanteosu/cse4471/vigilanteosuapp/FeedActivity.java
@@ -1,33 +1,119 @@
 package com.vigilanteosu.cse4471.vigilanteosuapp;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 
 
 public class FeedActivity extends ListActivity {
-    private final String getReportsUrl = "http://jeffcasavant.com:10100/vig/api/v1.0/list/reports?";
+    private final String getReportsUrl = "http://jeffcasavant.com:10100/vig/api/v1.0/list/reports";
     private final String numPosts = "15";
+    private final String startReportUUID = "00000000-0000-0000-0000-000000000000";
     SessionManagement session;
     /* WE NEED BUTTONS TO GET TO THE OTHER SCREEN... ALSO MIGHT BE OF VALUE TO CHECK THAT
     THE TOKEN IS VALID (METHOD DNE RN THO)*/
 
+    private String createUrl(String token, String reportUUID, String numPosts){
+        return getReportsUrl + String.format("?token=%s&reportid=%s&number_of_posts=%s", token, reportUUID, numPosts);
+    }
+
     private HashMap<String, String>[] getReports(String reportUUID){
-        HashMap<String, String>[] reports = new HashMap[15];
+        final HashMap<String, String>[] reports = new HashMap[15];
         SharedPreferences pref =
                 getSharedPreferences(SessionManagement.PREF_NAME,
                         MODE_PRIVATE);
-        String token = pref.getString("token", "");
+        String token = pref.getString("apiToken", "");
         // If the token is not set
         if(token.equals("")){
             //TODO error
         }
         String url = createUrl(token, reportUUID, numPosts);
+        final Context currentContext = this;
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //handle response
+                        if(response.has("reports")){
+                            JSONArray rps = null;
+                            try {
+                                rps = response.getJSONArray("reports");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            for(int i=0;i<rps.length();i++){
+                                HashMap<String, String> report = new HashMap<String, String>();
+                                JSONObject jsobj = null;
+                                try {
+                                    jsobj = (JSONObject)rps.get(i);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                if(jsobj.has("title")){
+                                    try {
+                                        report.put("title", jsobj.getString("title"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                if(jsobj.has("time")){
+                                    try {
+                                        report.put("time", jsobj.getString("time"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+//                                if(jsobj.has("description")){
+//                                    try {
+//                                        report.put("description", jsobj.getString("description"));
+//                                    } catch (JSONException e) {
+//                                        e.printStackTrace();
+//                                    }
+//                                }
+                                if(jsobj.has("severity")){
+                                    try {
+                                        report.put("severity", jsobj.getString("severity"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                if(jsobj.has("location")){
+                                    try {
+                                        report.put("location", jsobj.getString("location"));
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                                reports[i] = report;
+                            }
+                            FeedArrayAdapter faa = new FeedArrayAdapter(currentContext, reports);
+                            setListAdapter(faa);
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+        requestQueueSingleton.getInstance(this).addToRequestQueue(jsObjRequest);
         return reports;
     }
     @Override
@@ -38,8 +124,10 @@ public class FeedActivity extends ListActivity {
         setContentView(R.layout.activity_feed);
 
         session = new SessionManagement(getApplicationContext());
-
         session.checkLogin();
+
+        HashMap<String, String>[] reports = getReports(startReportUUID);
+
     }
 
 
