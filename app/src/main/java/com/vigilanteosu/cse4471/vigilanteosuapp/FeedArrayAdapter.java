2d5276694d888914f6d8e04a15b8418b132ee3ae
allenwhite <awhite23@rocketmail.com>
2014-12-02 17:11:51 -0500

    this bad. roll back immedietly

--- app/src/main/java/com/vigilanteosu/cse4471/vigilanteosuapp/FeedArrayAdapter.java
+++ app/src/main/java/com/vigilanteosu/cse4471/vigilanteosuapp/FeedArrayAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -55,8 +56,12 @@ public class FeedArrayAdapter extends ArrayAdapter<HashMap<String, String>> {
         reportLocation.setText(values.get(position).get("location"));
         reportTitle.setText(values.get(position).get("title"));
         final String desc = values.get(position).get("description");
+
+
         final String lon = values.get(position).get("lon");
         final String lat = values.get(position).get("lat");
+
+
 
 
         final int severity = Integer.parseInt(values.get(position).get("severity"));
@@ -90,8 +95,14 @@ public class FeedArrayAdapter extends ArrayAdapter<HashMap<String, String>> {
                 intent.putExtra("reportLocation", reportLocation.getText().toString());
                 intent.putExtra("severity", severity);
                 intent.putExtra("reportDesc", desc);
-                intent.putExtra("lon", lon);
-                intent.putExtra("lat", lat);
+
+                if(!lon.equals("None") && !lat.equals("None")){
+                    intent.putExtra("lon", lon);
+                    intent.putExtra("lat", lat);
+                }else{
+                    intent.putExtra("lon", "nah");
+                    intent.putExtra("lat", "nah");
+                }
                 context.startActivity(intent);
             }
         });
